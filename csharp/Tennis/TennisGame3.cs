4b7ac228850d8a22f5b0c45ad038ce53d3804a49
jihye son <jiiihyeee98@gmail.com>
2024-02-06 07:23:30 +0900

    [2] TennisGame2

--- csharp/Tennis/TennisGame3.cs
+++ csharp/Tennis/TennisGame3.cs
@@ -2,41 +2,44 @@ namespace Tennis
 {
     public class TennisGame3 : ITennisGame
     {
-        private int p2;
-        private int p1;
-        private string p1N;
-        private string p2N;
+        private int player2;
+        private int player1;
+        private string player1Socre;
+        private string player2Socre;
 
         public TennisGame3(string player1Name, string player2Name)
         {
-            this.p1N = player1Name;
-            this.p2N = player2Name;
+            this.player1Socre = player1Name;
+            this.player2Socre = player2Name;
         }
 
         public string GetScore()
         {
-            string s;
-            if ((p1 < 4 && p2 < 4) && (p1 + p2 < 6))
+            string tempScore = Empty.string;
+            string[] points = { "Love", "Fifteen", "Thirty", "Forty" };
+
+            if ((player1 < 4 && player2 < 4) && (player1 + player2 < 6))
             {
-                string[] p = { "Love", "Fifteen", "Thirty", "Forty" };
-                s = p[p1];
-                return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+                tempScore = points[player1];
+                return (player1.Equals(player1)) ? tempScore + "-All" : tempScore + "-" + points[player2];
             }
             else
             {
-                if (p1 == p2)
+                if (player1.Equals(player2))
                     return "Deuce";
-                s = p1 > p2 ? p1N : p2N;
-                return ((p1 - p2) * (p1 - p2) == 1) ? "Advantage " + s : "Win for " + s;
+                tempScore = player1 > player2 ? player1Socre : player2Socre;
+                return ((player1 - player2) * (player1 - player2) == 1) ? "Advantage " + tempScore : "Win for " + tempScore;
             }
         }
 
+        
+
         public void WonPoint(string playerName)
         {
-            if (playerName == "player1")
-                this.p1 += 1;
+            if (playerName.Equals("player1"))
+                this.player1 += 1;
             else
-                this.p2 += 1;
+                this.player2 += 1;
         }
 
     }

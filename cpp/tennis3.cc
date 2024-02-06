c770f09d0a921667c8a55bae26abe6fa5de023db
SJSong <ricassong@gmail.com>
2024-02-06 11:42:21 +0900

    c++  refactoring

--- cpp/tennis3.cc
+++ cpp/tennis3.cc
@@ -1,17 +1,39 @@
 #include <string>
+#include <cmath>
 
-const std::string tennis_score(int p1, int p2) {
-    std::string s;
-    std::string p1N = "player1";
-    std::string p2N = "player2";
-    if ((p1 < 4 && p2 < 4) && (p1 + p2 < 6)) {
-        std::string p[4] = {"Love", "Fifteen", "Thirty", "Forty"}; 
-        s = p[p1];
-        return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
-    } else {
+const std::string SCORES[4] = {"Love", "Fifteen", "Thirty", "Forty"};
+
+std::string tennis_score(int p1, int p2)
+{
+    auto resultForLeader = [](const std::string& leader, int scoreDifference) -> std::string {
+        if (scoreDifference == 1)
+        {
+            return "Advantage " + leader;
+        }
+        else
+        {
+            return "Win for " + leader;
+        }
+    };
+
+    if (p1 < 4 && p2 < 4 && (p1 + p2 < 6))
+    {
+        if (p1 == p2)
+        {
+            return SCORES[p1] + "-All";
+        }
+        else
+        {
+            return SCORES[p1] + "-" + SCORES[p2];
+        }
+    }
+    else
+    {
         if (p1 == p2)
             return "Deuce";
-        s = p1 > p2 ? p1N : p2N;
-        return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+        std::string leader  = p1 > p2 ? "player1" : "player2";
+        int  scoreDifference = std::abs(p1 - p2);
+        return resultForLeader(leader, scoreDifference);
     }
 }
+

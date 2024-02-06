1d5c1d67bd4f61b4d58d0c33335c3b4f2befaa11
SJSong <ricassong@gmail.com>
2024-02-06 14:01:05 +0900

    c# commit

--- csharp/Tennis/TennisGame3.cs
+++ csharp/Tennis/TennisGame3.cs
@@ -1,43 +1,55 @@
+using System;
+
 namespace Tennis
 {
     public class TennisGame3 : ITennisGame
     {
-        private int p2;
-        private int p1;
-        private string p1N;
-        private string p2N;
+        private int p1Score;
+        private int p2Score;
+        private readonly string p1Name;
+        private readonly string p2Name;
+        private static readonly string[] scoreNames = { "Love", "Fifteen", "Thirty", "Forty" };
+        private const int EarlyGameScoreLimit = 4;
+        private const int MaxPreDeuceScoreSum = 6;
 
         public TennisGame3(string player1Name, string player2Name)
         {
-            this.p1N = player1Name;
-            this.p2N = player2Name;
+            this.p1Name = player1Name;
+            this.p2Name = player2Name;
         }
 
         public string GetScore()
         {
-            string s;
-            if ((p1 < 4 && p2 < 4) && (p1 + p2 < 6))
-            {
-                string[] p = { "Love", "Fifteen", "Thirty", "Forty" };
-                s = p[p1];
-                return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
-            }
-            else
-            {
-                if (p1 == p2)
-                    return "Deuce";
-                s = p1 > p2 ? p1N : p2N;
-                return ((p1 - p2) * (p1 - p2) == 1) ? "Advantage " + s : "Win for " + s;
-            }
+            if (IsEarlyGame())
+                return EarlyGameScore();
+            return EndGameScore();
         }
 
+        private bool IsEarlyGame() => p1Score < EarlyGameScoreLimit && p2Score < EarlyGameScoreLimit && p1Score + p2Score < MaxPreDeuceScoreSum;
+
+        private string EarlyGameScore()
+        {
+            if (p1Score == p2Score)
+                return scoreNames[p1Score] + "-All";
+            return scoreNames[p1Score] + "-" + scoreNames[p2Score];
+        }
+
+        private string EndGameScore()
+        {
+            var scoreDifference = Math.Abs(p1Score - p2Score);
+            var leaderName = p1Score > p2Score ? p1Name : p2Name;
+            return scoreDifference switch
+            {
+                1 => $"Advantage {leaderName}",
+                _ => $"Win for {leaderName}",
+            };
+        }
         public void WonPoint(string playerName)
         {
-            if (playerName == "player1")
-                this.p1 += 1;
+            if (playerName == p1Name)
+                p1Score++;
             else
-                this.p2 += 1;
+                p2Score++;
         }
-
     }
 }

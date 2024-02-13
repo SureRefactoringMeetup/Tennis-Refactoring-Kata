34e47f1b58cc279e3e1b0a3b5bfb67d9d0ea82ff
sdhan <sdhan@suresofttech.com>
2024-02-13 12:18:20 +0900

    diff 파일 생성을 위한 Tennis3 원복
    diff 파일 생성을 위한 Tennis3 원복

--- java/src/main/java/han/tennisgame/TennisGame3.java
+++ java/src/main/java/han/tennisgame/TennisGame3.java
@@ -1,6 +1,4 @@
-package han.tennisgame;
-
-public class TennisGame3 implements TennisGame{
+public class TennisGame3 implements TennisGame {
 
     private int p2;
     private int p1;
@@ -11,19 +9,26 @@ public class TennisGame3 implements TennisGame{
         this.p1N = p1N;
         this.p2N = p2N;
     }
-    public String getP1N() {
-        return p1N;
-    }
-    public String getP2N() {
-        return p2N;
-    }
-    public void wonPoint(String playerName) {
-        if (playerName.equals("player1")) this.p1 += 1;
-        else this.p2 += 1;
-    }
+
     public String getScore() {
-        return TennisScoreDelegator.getInstance()
-                    .createTennisScoreHandler(this)
-                    .getScoreResult(p1, p2);
+        String s;
+        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
+            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+            s = p[p1];
+            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+        } else {
+            if (p1 == p2)
+                return "Deuce";
+            s = p1 > p2 ? p1N : p2N;
+            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+        }
+    }
+
+    public void wonPoint(String playerName) {
+        if (playerName == "player1")
+            this.p1 += 1;
+        else
+            this.p2 += 1;
+
     }
 }
